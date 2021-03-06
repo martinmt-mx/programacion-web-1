cd19f95055e20a7d5c9eeba2324dafd6f074eb9c
martinmt-mx <mmojica0@ucol.mx>
2021-03-05 19:19:27 -0600

    arrow functions

---
+++ first_class/funciones/app.js
@@ -0,0 +1,39 @@
+//Originales
+function greet(){
+    console.log('Hello')
+}
+greet()
+
+function logGreeting(fn){
+    fn()
+}
+logGreeting(greet)
+
+let greetMe = function(){
+    console.log('Hello from the function expression')
+}
+greetMe()
+
+logGreeting(greetMe)
+
+//Funcion flecha ejemplo
+let mifuncion = () => {
+    console.log('si jalo la funcion')
+}
+mifuncion()
+
+//Refactor de funciones de acuerdo a las especificaiones de ES6
+let refGreet = () => {
+    console.log('Hello')
+}
+refGreet()
+
+let refLogGreeting = (fn) =>{
+    fn()
+}
+refLogGreeting(refGreet)
+
+let refGreetMe = () => {
+    console.log('Hello from the function expression')
+}
+refGreetMe()
